a6600c3c2f85274b5639efa72a35901f51655792
Scooter <scooter_zav@mail.ru>
2019-12-10 21:37:14 +0300

    Revert "PopUp added v2"
    This reverts commit dd0b2cc5a849491f9d11655e4f3e84ec74cc368c.

--- js/popup.js
+++ js/popup.js
@@ -2,9 +2,15 @@ export class PopUp {
     constructor(Selector) {
         this.wind = document.querySelector(Selector);
         this.btnClose = this.wind.querySelector('.popup-close');
-        this.btnClose.addEventListener('click',()=>this.hide());
+        this.btnClose.addEventListener('click',()=>{
+            this.hide();
+        });
     }
 
-    show() {this.wind.style.display = 'block';}
-    hide() {this.wind.style.display = 'none';}
+    show() {
+        this.wind.style.display = 'block';
+    }
+    hide() {
+        this.wind.style.display = 'none';
+    }
 }
