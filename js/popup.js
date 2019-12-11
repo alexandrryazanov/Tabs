03e509d8b8dea34d8ddae0383362c0c5a95d16d6
Scooter <scooter_zav@mail.ru>
2019-12-11 23:36:19 +0300

    Added FormData send

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,42 @@
 export class PopUp {
     constructor(Selector) {
         this.wind = document.querySelector(Selector);
+        this.form = this.wind.querySelector('form');
         this.btnClose = this.wind.querySelector('.popup-close');
-        this.btnClose.addEventListener('click',()=>{
-            this.hide();
-        });
+        this.btnClose.addEventListener('click',()=>this.hide());
+        this.btnOk = this.wind.querySelector('button');
+        this.form.addEventListener('submit',(event)=>this.frmSubmit(event));
+        this.tel = this.wind.querySelectorAll('input')[0];
+
+        this.request = new XMLHttpRequest();
+        this.request.addEventListener('readystatechange',()=>this.getServerStatus());
     }
 
-    show() {
-        this.wind.style.display = 'block';
+    show() {this.wind.style.display = 'block';document.body.style.overflow='hidden';}
+    hide() {this.wind.style.display = 'none';document.body.style.overflow=''}
+
+    frmSubmit(e) {
+        e.preventDefault();
+        
+        //--получаем данные из формы
+        let frmData = new FormData(this.form), 
+            frmDataObj = {};                                
+        frmData.forEach((value,key) => frmDataObj[key] = value);//--Преобразуем в объект для дальнейшего преобразование в JSON
+        let frmDataJSON = JSON.stringify(frmDataObj);           //--JSON
+
+        //--Коннектимся и передаем
+        this.request.open('GET', 'server.php');
+        this.request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        this.request.send(frmDataJSON);
     }
-    hide() {
-        this.wind.style.display = 'none';
+
+    getServerStatus() {
+        if (this.request.readyState==4 && this.request.status==200) {
+            swal("Заявка отправлена!", "Мы свяжемся с Вами в ближайшее время!", "success")
+                .then(()=>{
+                    this.tel.value = '';
+                });
+            
+        }
     }
 }
